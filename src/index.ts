9f4d48a7d5d47f6fa652e876e87b7d83135639b5
Benedikt Lüken-Winkels <belu02@dfki.de>
2023-01-26 12:45:20 +0100

    Add Some Style

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,54 @@
 #!/usr/bin/env node
 
 import yargs from "yargs/yargs";
-import {hideBin} from "yargs/helpers";
-import notifier from "node-notifier"
-
+import { hideBin } from "yargs/helpers";
+import notifier from "node-notifier";
+import cliProgress from "cli-progress";
+import colors from "ansi-colors";
 
 const argv = yargs(hideBin(process.argv))
   .options({
-    t: {type: "number"},
+    time: {
+      type: "number",
+      demandOption: true,
+      alias: "t",
+      description: "Minutes your tea needs",
+    },
   })
-  .argv
+  .parseSync();
 
+const timeInSecs = argv.time * 60;
 
-await delay(argv.t * 1000);
+// create new progress bar
+const b1 = new cliProgress.SingleBar({
+  format:
+    "TEAmer |" +
+    colors.green("{bar}") +
+    "| {percentage}% | {value}/{total} Sekunden",
+  barCompleteChar: "\u2588",
+  barIncompleteChar: "\u2591",
+  hideCursor: true,
+});
+b1.start(timeInSecs, 0, {
+  speed: "N/A",
+});
+
+let running = true;
+while (running) {
+  await delay(1000);
+  b1.increment();
+  running = b1.getProgress() < 1;
+}
+
+// stop the bar
+b1.stop();
+
 notifier.notify({
   title: "Tee ist fertig",
-  message: `Ziehzeit war ${argv.t} min`,
-  timeout: 30,
-  wait: false
-})
-
+  message: `Ziehzeit war min`,
+  wait: false,
+  timeout: false,
+});
 
 function delay(ms: number) {
   return new Promise((resolve) => setTimeout(resolve, ms));
