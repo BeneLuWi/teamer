ef29a749136dafbe23567e91d795255cab98704d
Benedikt Lüken-Winkels <belu02@dfki.de>
2023-01-26 10:49:34 +0100

    Minimal example

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,19 @@
 #!/usr/bin/env node
 
-process.stdout.write("Whaddup")
+import yargs from "yargs/yargs";
+import {hideBin} from "yargs/helpers";
+
+const argv = yargs(hideBin(process.argv))
+  .options({
+    t: {type: "number"},
+  })
+  .argv
+
+
+await delay(1000);
+console.log(argv.t);
+
+
+function delay(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
